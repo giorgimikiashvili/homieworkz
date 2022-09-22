93387e2e66a12dc65952d9bd63c870235e58fb99
giorgimikiashvili <giorgimikiashvili255@gmail.com>
2022-09-22 04:00:31 +0400

    new commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,16 @@
+name = "abdul" #string tipis cvladi
+age = 22 #int tipis cvladi (integer - mteli ricxvi)
+height = 184.3 #float tipis cvladi (float-atwiladi)
+knows_programming = True #bool tipis cvladi (boolean - logikuri cvladi, ormagi mnishvnelobis)
+is_chad = False #bool
+#name aris cvladi
+# = cvladis inicializacia
+# "nika" aris cvladis mnishvneloba
+surname = "jabbar"
+print(name)
+
+
+
+
+
+print("my name is " + str(name)+ " " + str(surname) + " " + "I'm" + " " + str(age) + " years old" + " and I'm " + str(height) + " cm tall")
